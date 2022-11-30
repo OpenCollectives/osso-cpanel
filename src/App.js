b5840b6547500511ad502f3428619b3dabcc3a3a
rigel-star <boyzdod@gmail.com>
2022-11-30 17:53:36 +0545

    init folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './layouts/Navbar';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from './pages/home/index';
+import "./assets/global.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='container'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} exact />
+          <Route path="/books/create" element={<span>Hello books</span>} exact />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Navbar extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return (<ul>
+            <li>
+                Random
+            </li>
+        </ul>);
+    }
+}
